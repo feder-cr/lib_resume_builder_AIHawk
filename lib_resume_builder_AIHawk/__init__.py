67b4f19ca955c29c64d9dd79dda772dea7e4a96c
feder-cr <federico.elia.majo@gmail.com>
2024-08-16 19:44:28 +0100

    first commit

---
+++ lib_resume_builder_AIHawk/__init__.py
@@ -0,0 +1,7 @@
+__version__ = '0.1'
+
+from .config import Config
+from .resume_generator import ResumeGenerator
+from .style_manager import StyleManager
+from .manager_facade import FacadeManager
+from .resume import Resume
---
+++ setup.py
@@ -0,0 +1,36 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='lib_resume_builder_AIHawk',
+    version='0.1',
+    description='A package to generate AI-assisted resumes using GPT models',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author='feder-cr',
+    author_email='federico.elia.majo@gmail.com',
+    url='https://github.com/feder-cr/cv_ai_hawk',
+    packages=find_packages(),
+    install_requires=[ 
+        'langchain==0.2.11',
+        'langchain-community==0.2.10',
+        'langchain-core==0.2.24',
+        'langchain-openai==0.1.17',
+        'langchain-text-splitters==0.2.2',
+        'langsmith==0.1.93',
+        'openai==1.37.1',
+        'regex==2024.7.24',
+        'selenium==4.9.1',
+        'webdriver-manager==4.0.2',
+        'inquirer',
+        'langchainhub',
+        'unstructured',
+        'faiss-cpu',
+    ],
+    classifiers=[  # Classificatori che descrivono il pacchetto
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.10',  
+    include_package_data=True,  # Include altri file indicati nel MANIFEST.in
+)
