74539f948cda29e72173a85aa3891682a2b3962d
feder-cr <federico.elia.majo@gmail.com>
2024-08-18 23:30:46 +0100

    separata la gestione dello stile, cois da non doverlo decidere sempre, mas olo una volta

--- lib_resume_builder_AIHawk/manager_facade.py
+++ lib_resume_builder_AIHawk/manager_facade.py
@@ -20,6 +20,7 @@ class FacadeManager:
         self.style_manager.set_styles_directory(global_config.STYLES_DIRECTORY)
         self.resume_generator = resume_generator
         self.resume_generator.set_resume_object(resume_object)
+        self.selected_style = None  # Proprietà per memorizzare lo stile selezionato
 
     def prompt_user(self, choices: list[str], message: str) -> str:
         questions = [
@@ -39,10 +40,7 @@ class FacadeManager:
         ]
         return inquirer.prompt(questions)['text']
 
-    def pdf_base64(self, job_description_url=None, job_description_text=None):
-        if (job_description_url is not None and job_description_text is not None):
-            raise ValueError("Esattamente uno tra 'job_description_url' o 'job_description_text' deve essere fornito.")
-        
+    def choose_style(self):
         styles = self.style_manager.get_styles()
         if not styles:
             print("No styles available")
@@ -50,15 +48,22 @@ class FacadeManager:
 
         formatted_choices = self.style_manager.format_choices(styles)
         selected_choice = self.prompt_user(formatted_choices, "Which style would you like to adopt?")
-        selected_style = selected_choice.split(' (')[0]
-        style_path = self.style_manager.get_style_path(selected_style)
+        self.selected_style = selected_choice.split(' (')[0]
+
+    def pdf_base64(self, job_description_url=None, job_description_text=None):
+        if (job_description_url is not None and job_description_text is not None):
+            raise ValueError("Esattamente uno tra 'job_description_url' o 'job_description_text' deve essere fornito.")
+        
+        if self.selected_style is None:
+            raise ValueError("Devi scegliere uno stile prima di generare il PDF.")
+        
+        style_path = self.style_manager.get_style_path(self.selected_style)
 
         with tempfile.NamedTemporaryFile(delete=False, mode='w', suffix='.html', encoding='utf-8') as temp_html_file:
             temp_html_path = temp_html_file.name
             if job_description_url is None and job_description_text is None:
                 self.resume_generator.create_resume(style_path, temp_html_path)
             elif job_description_url is not None and job_description_text is None:
-                job_description_url = self.prompt_for_url("Please enter the URL of the job description:")
                 self.resume_generator.create_resume_job_description_url(style_path, job_description_url, temp_html_path)
             elif job_description_url is None and job_description_text is not None:
                 self.resume_generator.create_resume_job_description_text(style_path, job_description_text, temp_html_path)
