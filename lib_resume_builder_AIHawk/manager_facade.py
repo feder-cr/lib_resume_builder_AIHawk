cc556878cd8574cf668ca86ed2d5d7cbd5a0ee25
feder-cr <federico.elia.majo@gmail.com>
2024-08-18 15:40:05 +0100

    separation logic

--- lib_resume_builder_AIHawk/manager_facade.py
+++ lib_resume_builder_AIHawk/manager_facade.py
@@ -7,35 +7,19 @@ from lib_resume_builder_AIHawk.config import global_config
 from lib_resume_builder_AIHawk.utils import HTML_to_PDF
 
 class FacadeManager:
-    def __init__(self, api_key, style_manager, resume_generator, resume_object, log_path, job_description_url=None, selection=None, job_description_text=None):
+    def __init__(self, api_key, style_manager, resume_generator, resume_object, log_path):
         # Ottieni il percorso assoluto della directory della libreria
         lib_directory = Path(__file__).resolve().parent
-
         global_config.STRINGS_MODULE_RESUME_PATH = lib_directory / "resume_prompt/strings_feder-cr.py"
         global_config.STRINGS_MODULE_RESUME_JOB_DESCRIPTION_PATH = lib_directory / "resume_job_description_prompt/strings_feder-cr.py"
         global_config.STRINGS_MODULE_NAME = "strings_feder_cr"
         global_config.STYLES_DIRECTORY = lib_directory / "resume_style"
         global_config.LOG_OUTPUT_FILE_PATH = log_path
         global_config.API_KEY = api_key
-        
         self.style_manager = style_manager
         self.style_manager.set_styles_directory(global_config.STYLES_DIRECTORY)
         self.resume_generator = resume_generator
         self.resume_generator.set_resume_object(resume_object)
-        
-
-        if (job_description_url is not None and job_description_text is not None):
-            raise ValueError("Esattamente uno tra 'job_description_url' o 'job_description_text' deve essere fornito.")
-
-        self.job_description_url = job_description_url
-        self.job_description_text = job_description_text
-        self.selected_action = selection
-
-    def set_job_description_url(self, url):
-        self.job_description_url = url
-
-    def set_job_description_text(self, text):
-        self.job_description_text = text
 
     def prompt_user(self, choices: list[str], message: str) -> str:
         questions = [
@@ -55,14 +39,10 @@ class FacadeManager:
         ]
         return inquirer.prompt(questions)['text']
 
-    def pdf_base64(self):
-        if self.selected_action is None:
-            self.selected_action = self.prompt_user(['Create Resume', 'Create Resume based on Job Description URL', 'Exit'], "What would you like to do?")
+    def pdf_base64(self, job_description_url=None, selection=None, job_description_text=None):
+        if (job_description_url is not None and job_description_text is not None):
+            raise ValueError("Esattamente uno tra 'job_description_url' o 'job_description_text' deve essere fornito.")
         
-        if self.selected_action == 'Exit':
-            print("Exiting...")
-            return None
-
         styles = self.style_manager.get_styles()
         if not styles:
             print("No styles available")
@@ -75,15 +55,15 @@ class FacadeManager:
 
         with tempfile.NamedTemporaryFile(delete=False, mode='w', suffix='.html', encoding='utf-8') as temp_html_file:
             temp_html_path = temp_html_file.name
-            if self.selected_action == 'Create Resume':
+            if selection == 'Create Resume':
                 self.resume_generator.create_resume(style_path, temp_html_path)
-            elif self.selected_action == 'Create Resume based on Job Description URL':
-                if self.job_description_url is None:
-                    self.job_description_url = self.prompt_for_url("Please enter the URL of the job description:")
-                self.resume_generator.create_resume_job_description_url(style_path, self.job_description_url, temp_html_path)
-            elif self.selected_action == 'Create Resume based on Job Description Text':
-                self.resume_generator.create_resume_job_description_text(style_path, self.job_description_text, temp_html_path)
-            elif self.selected_action == 'Exit':
+            elif selection == 'Create Resume based on Job Description URL':
+                if job_description_url is None:
+                    job_description_url = self.prompt_for_url("Please enter the URL of the job description:")
+                self.resume_generator.create_resume_job_description_url(style_path, job_description_url, temp_html_path)
+            elif selection == 'Create Resume based on Job Description Text':
+                self.resume_generator.create_resume_job_description_text(style_path, job_description_text, temp_html_path)
+            elif selection == 'Exit':
                 exit()
         pdf_base64 = HTML_to_PDF(temp_html_path)
         os.remove(temp_html_path)
